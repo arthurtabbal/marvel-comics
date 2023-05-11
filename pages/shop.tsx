7b72d657ad579850115d0dc6a7128485aa1ac00d
Arthur Tabbal <arthurtabbal@gmail.com>
2023-05-11 15:38:57 -0300

    create shop page consuming marvel API

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,3 +1,42 @@
-export default function Shop() {
-  return <h1>Shop</h1>;
+import loadAllComics, { Comics } from '@src/comics/load-all-comics'
+
+import ComicsCard from '@components/ComicsCard'
+import styled from 'styled-components'
+
+interface Props {
+  comics: Comics[]
+}
+
+const Title = styled.h1`
+  font-size: 2rem;
+  text-transform: uppercase;
+  margin: 0 1rem 1rem;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+  gap: 1.5rem 0.5rem;
+  place-items: center;
+`
+
+const ShopPage: React.FC<Props> = (props) => {
+  const { comics } = props
+  return (
+    <div>
+      <Title>All Comics</Title>
+      <Grid>
+        {comics.map((comic) => (
+          <ComicsCard key={comic.id} comics={comic} />
+        ))}
+      </Grid>
+    </div>
+  )
+}
+
+export default ShopPage
+
+export async function getStaticProps() {
+  const comics = await loadAllComics()
+  return { props: { comics } }
 }
