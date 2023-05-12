36a0948f6bf9882ceb8489a762e9446f1417299b
Arthur Tabbal <arthurtabbal@gmail.com>
2023-05-12 12:27:21 -0300

    add type animation to home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { styled } from 'styled-components'
+import { TypeAnimation } from 'react-type-animation'
+
+const Container = styled.div`
+  text-align: center;
+`
+
 export default function Home() {
+  const sequence = [
+    'Marvel Comics Store',
+    1500,
+    '',
+    1000,
+    'All your favorite heroes are here',
+    1500,
+    '',
+    1000,
+    'Shop now and enjoy!',
+    4000,
+  ]
   return (
-    <>
-      <h1>Testing Git Commit</h1>
-    </>
+    <Container>
+      <h2>
+        <TypeAnimation
+          sequence={sequence}
+          repeat={Infinity}
+          speed={40}
+          deletionSpeed={80}
+        ></TypeAnimation>
+      </h2>
+    </Container>
   )
 }
